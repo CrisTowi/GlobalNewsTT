5853bdcfda409aff5016c6b0128c2cbd73c4da83
CrisTowi <christian_consuelo@outlook.com>
2015-02-09 16:09:56 -0600

    Notificacioes globales y al eliminar mensaje

---
+++ GlobalNews/static/js/base.js
@@ -0,0 +1,24 @@
+var host = "localhost";
+
+var socket = io.connect(host + ":3000");
+
+//Cuando llegue un nuevo mensaje
+socket.on('nuevo_comentario', function(data){
+    console.log(data);
+    var n = noty({
+        text: '<span class="texto_noty" data-id_nota="' + data.nota_id + '"> Nuevo Comentario: <br>' + data.contenido + ' de ' + data.usuario + '<br> en ' + data.nota + '</span>',
+        layout: 'bottomRight',
+        type: 'information',
+        timeout: 3500,
+        callback: { onClose: function() { /* Go to the URL i want to go */ }},
+        animation: {
+            open: {height: 'toggle'}, // jQuery animate function property object
+            close: {height: 'toggle'}, // jQuery animate function property object
+            easing: 'swing', // easing
+            speed: 500 // opening & closing animation speed
+        }
+    });
+    $('.texto_noty').on('click', function(){
+        window.location.href = '/publicacion/' + $(this).data('id_nota');
+    });
+});
