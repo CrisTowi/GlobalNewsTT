c60ca1d5367048ba2c077233c6ac7c7e474e4f49
CrisTowi <christian_consuelo@outlook.com>
2015-04-23 23:19:44 -0500

    Prueba

--- Principal/serializers.py
+++ Principal/serializers.py
@@ -1,4 +1,4 @@
-from Principal.models import Usuario, Nota, ReporteUsuario, ReporteNota, Seccion, UsuarioSigueUsuario, Comentario
+from Principal.models import Usuario, Nota, ReporteUsuario, ReporteNota, Seccion, UsuarioSigueUsuario, Comentario, UsuarioSigueSeccion
 from rest_framework import serializers
 
 
@@ -11,13 +11,18 @@ class UsuarioSigueUsuarioSerializer(serializers.ModelSerializer):
     class Meta:
         model = UsuarioSigueUsuario
 
+class UsuarioSigueSeccionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UsuarioSigueSeccion
+
 class NotaSerializer(serializers.ModelSerializer):
 
-    usuario = serializers.Field(source='usuario.username')
-    fecha = serializers.Field(source='formato_fecha')
-    seccion = serializers.Field(source='subseccion.seccion.id')
-    likes = serializers.Field(source='num_likes')
-    comentarios = serializers.Field(source='get_comentarios')
+    usuario = serializers.ReadOnlyField(source='usuario.username')
+    fecha = serializers.ReadOnlyField(source='formato_fecha')
+    seccion = serializers.ReadOnlyField(source='subseccion.seccion.id')
+    subseccion = serializers.ReadOnlyField(source='subseccion.id')
+    likes = serializers.ReadOnlyField(source='get_likes')
+    comentarios = serializers.ReadOnlyField(source='get_comentarios')
     
     class Meta:
         model = Nota
@@ -42,4 +47,4 @@ class ReporteNotaSerializer(serializers.HyperlinkedModelSerializer):
 class SeccionSerializer(serializers.HyperlinkedModelSerializer):
 	class Meta:
 		model = Seccion
-		fields = ('url','nombre',)
+		fields = ('url','nombre','imagen',)
