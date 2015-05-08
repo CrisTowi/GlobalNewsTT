a9bb9e2e3c273815900d7e0e2482ee9cc3809dd3
CrisTowi <christian_consuelo@outlook.com>
2015-05-07 22:42:47 -0500

    Agregar Geolocalizacion A Publiaciones

--- GlobalNews/static/js/base.js
+++ GlobalNews/static/js/base.js
@@ -12,63 +12,180 @@ if (navigator.geolocation) {
           lon: lon
         }
         var socket = io.connect(host + ":3000", {query:parameters});
+        //Cuando llegue un nuevo mensaje
+        socket.on('nuevo_comentario', function(data){
+            var n = noty({
+                text: '<span class="texto_noty" data-id_nota="' + data.nota_id + '"> Nuevo Comentario: <br>' + data.contenido + ' de ' + data.usuario + '<br> en ' + data.nota + '</span>',
+                layout: 'bottomRight',
+                type: 'information',
+                timeout: 3500,
+                animation: {
+                    open: {height: 'toggle'}, // jQuery animate function property object
+                    close: {height: 'toggle'}, // jQuery animate function property object
+                    easing: 'swing', // easing
+                    speed: 500 // opening & closing animation speed
+                }
+            });
+            $('.texto_noty').on('click', function(){
+                window.location.href = '/publicacion/' + $(this).data('id_nota');
+            });
+        });
+
+        socket.on('me_gusta', function(data){
+            var n = noty({
+                text: '<span class="texto_noty" data-id_nota="' + data.nota_id + '"> A: ' + data.usuario + ' le gusta ' + data.nota + '</span>',
+                layout: 'bottomRight',
+                type: 'information',
+                timeout: 3500,
+                animation: {
+                    open: {height: 'toggle'}, // jQuery animate function property object
+                    close: {height: 'toggle'}, // jQuery animate function property object
+                    easing: 'swing', // easing
+                    speed: 500 // opening & closing animation speed
+                }
+            });
+            $('.texto_noty').on('click', function(){
+                window.location.href = '/publicacion/' + $(this).data('id_nota');
+            });
+        });
+
+        socket.on('nuevo_seguidor', function(data){
+            var n = noty({
+                text: '<span class="texto_noty_seguidor" data-id_usuario="' + data.seguidor_id + '">¡Nuevo seguidor! <br>' + data.seguidor + ' te ha seguido </span>',
+                layout: 'bottomRight',
+                type: 'success',
+                timeout: 3500,
+                callback: { onClose: function() { /* Go to the URL i want to go */ }},
+                animation: {
+                    open: {height: 'toggle'}, // jQuery animate function property object
+                    close: {height: 'toggle'}, // jQuery animate function property object
+                    easing: 'swing', // easing
+                    speed: 500 // opening & closing animation speed
+                }
+            });
+            $('.texto_noty_seguidor').on('click', function(){
+                window.location.href = '/perfil/' + $(this).data('id_usuario');
+            });
+        });
+        var left_right = 1;
+        
+        socket.on('nueva_publicacion', function(data){
+
+            var ubicacion = '';
+
+            if(left_right == 1){
+                ubicacion = ' class="timeline-inverted"';
+                left_right = 0;
+            } else {
+                ubicacion = '';
+                left_right = 1;
+            }
+
+            var template = $('<li' + ubicacion + '> \
+                                        <div class="timeline-badge"><i class="fa fa-check"></i> \
+                                        </div> \
+                                        <div class="timeline-panel"> \
+                                            <div class="timeline-heading"> \
+                                                <a href="/publicacion/' + data.id + '"><h4 class="timeline-title">' + data.titulo + '</h4></a> \
+                                                <a href="/perfil/' + data.usuario_id + '"><h5>Por ' + data.usuario + '</h5></a> \
+                                                <p> \
+                                                    <small class="text-muted"><i class="fa fa-time"></i>' + data.fecha + '</small> \
+                                                </p> \
+                                            </div> \
+                                            <div class="timeline-body"> \
+                                                <p>' + data.descripcion + '</p> \
+                                            </div> \
+                                        </div> \
+                                    </li>');
+            template.hide();
+            $('#timeline').prepend(template);
+            template.show('slow');
+
+
+            var marker1 = new google.maps.Marker({
+                position: new google.maps.LatLng(data.latitud,data.longitud),
+                map: map,
+                title: data.titulo,
+                url: '/publicacion/' + data.id
+                });
+                google.maps.event.addListener(marker1, 'click', function() {
+                window.location.href = this.url;
+            });
+            var n = noty({
+                text: '<span class="texto_noty" data-id_nota="' + data.id + '"> Una noticia que te puede interesar </span>',
+                layout: 'bottomRight',
+                type: 'information',
+                timeout: 3500,
+                animation: {
+                    open: {height: 'toggle'}, // jQuery animate function property object
+                    close: {height: 'toggle'}, // jQuery animate function property object
+                    easing: 'swing', // easing
+                    speed: 500 // opening & closing animation speed
+                }
+            });
+            $('.texto_noty').on('click', function(){
+                window.location.href = '/publicacion/' + $(this).data('id_nota');
+            });
+        });
+        socket.on('nueva_publicacion_localizacion', function(data){
+
+            var ubicacion = '';
+
+            if(left_right == 1){
+                ubicacion = ' class="timeline-inverted"';
+                left_right = 0;
+            } else {
+                ubicacion = '';
+                left_right = 1;
+            }
+
+            var template = $('<li' + ubicacion + '> \
+                                        <div class="timeline-badge"><i class="fa fa-check"></i> \
+                                        </div> \
+                                        <div class="timeline-panel"> \
+                                            <div class="timeline-heading"> \
+                                                <a href="/publicacion/' + data.id + '"><h4 class="timeline-title">' + data.titulo + '</h4></a> \
+                                                <a href="/perfil/' + data.usuario_id + '"><h5>Por ' + data.usuario + '</h5></a> \
+                                                <p> \
+                                                    <small class="text-muted"><i class="fa fa-time"></i>' + data.fecha + '</small> \
+                                                </p> \
+                                            </div> \
+                                            <div class="timeline-body"> \
+                                                <p>' + data.descripcion + '</p> \
+                                            </div> \
+                                        </div> \
+                                    </li>');
+            template.hide();
+            $('#timeline').prepend(template);
+            template.show('slow');
+
+
+            var marker1 = new google.maps.Marker({
+                position: new google.maps.LatLng(data.latitud,data.longitud),
+                map: map,
+                title: data.titulo,
+                url: '/publicacion/' + data.id
+                });
+                google.maps.event.addListener(marker1, 'click', function() {
+                window.location.href = this.url;
+            });
+            var n = noty({
+                text: '<span class="texto_noty" data-id_nota="' + data.id + '"> Una nueva noticia cercana a ti </span>',
+                layout: 'bottomRight',
+                type: 'information',
+                timeout: 3500,
+                animation: {
+                    open: {height: 'toggle'}, // jQuery animate function property object
+                    close: {height: 'toggle'}, // jQuery animate function property object
+                    easing: 'swing', // easing
+                    speed: 500 // opening & closing animation speed
+                }
+            });
+            $('.texto_noty').on('click', function(){
+                window.location.href = '/publicacion/' + $(this).data('id_nota');
+            });
+
+        });
     });
 }
 
-//Cuando llegue un nuevo mensaje
-socket.on('nuevo_comentario', function(data){
-    var n = noty({
-        text: '<span class="texto_noty" data-id_nota="' + data.nota_id + '"> Nuevo Comentario: <br>' + data.contenido + ' de ' + data.usuario + '<br> en ' + data.nota + '</span>',
-        layout: 'bottomRight',
-        type: 'information',
-        timeout: 3500,
-        animation: {
-            open: {height: 'toggle'}, // jQuery animate function property object
-            close: {height: 'toggle'}, // jQuery animate function property object
-            easing: 'swing', // easing
-            speed: 500 // opening & closing animation speed
-        }
-    });
-    $('.texto_noty').on('click', function(){
-        window.location.href = '/publicacion/' + $(this).data('id_nota');
-    });
-});
-
-socket.on('me_gusta', function(data){
-    var n = noty({
-        text: '<span class="texto_noty" data-id_nota="' + data.nota_id + '"> A: ' + data.usuario + ' le gusta ' + data.nota + '</span>',
-        layout: 'bottomRight',
-        type: 'information',
-        timeout: 3500,
-        animation: {
-            open: {height: 'toggle'}, // jQuery animate function property object
-            close: {height: 'toggle'}, // jQuery animate function property object
-            easing: 'swing', // easing
-            speed: 500 // opening & closing animation speed
-        }
-    });
-    $('.texto_noty').on('click', function(){
-        window.location.href = '/publicacion/' + $(this).data('id_nota');
-    });
-});
-
-socket.on('nuevo_seguidor', function(data){
-
-
-    var n = noty({
-        text: '<span class="texto_noty_seguidor" data-id_usuario="' + data.seguidor_id + '">¡Nuevo seguidor! <br>' + data.seguidor + ' te ha seguido </span>',
-        layout: 'bottomRight',
-        type: 'success',
-        timeout: 3500,
-        callback: { onClose: function() { /* Go to the URL i want to go */ }},
-        animation: {
-            open: {height: 'toggle'}, // jQuery animate function property object
-            close: {height: 'toggle'}, // jQuery animate function property object
-            easing: 'swing', // easing
-            speed: 500 // opening & closing animation speed
-        }
-    });
-    $('.texto_noty_seguidor').on('click', function(){
-        window.location.href = '/perfil/' + $(this).data('id_usuario');
-    });
-});
--- node/server.js
+++ node/server.js
@@ -13,82 +13,99 @@ var usuarios = [];
 var redis = require('redis');
 var sub = redis.createClient();
 
-//Funcion que maneja cada evento enviado desde Django
-var recibeMensaje = function(channel, message){
-    var i,j;
-    var datos = JSON.parse(message);
-    switch (channel) {
-        case 'chat':
-            io.to('chat_' + datos.id_chat ).emit('mensaje_entrada', datos);
-            break;
-
-        case 'publicacion':
-
-            for(j=0; j<datos.lista_usuarios.length; j++){            
-                for(i=0; i<usuarios.length; i++){
-                    if(usuarios[i].session_id == datos.lista_usuarios[j]){
-                        io.to(usuarios[i].socket_id).emit('nueva_publicacion', datos);
-                    }
-                }
-            }
-            break;
-
-        case 'comentario':
-            for(i=0; i<usuarios.length; i++){
-                if(usuarios[i].session_id == datos.session_key){
-                    io.to(usuarios[i].socket_id).emit('nuevo_comentario', datos);
-                }
-            }
-            break;
-
-        case 'me_gusta':
-            for(i=0; i<usuarios.length; i++){
-                if(usuarios[i].session_id == datos.session_key){
-                    io.to(usuarios[i].socket_id).emit('me_gusta', datos);
-                }
-            }
-            break;
-
-        case 'nuevo_seguidor':
-            for(i=0; i<usuarios.length; i++){
-                if(usuarios[i].session_id == datos.session_key){
-                    io.to(usuarios[i].socket_id).emit('nuevo_seguidor', datos);
-                }
-            }
-            break;
-    }
-};
-
-//Subscribe a los diferentes canales
-sub.subscribe('chat');
-sub.subscribe('publicacion');
-sub.subscribe('comentario');
-sub.subscribe('nuevo_seguidor');
-sub.subscribe('me_gusta');
-
-//Evento que recibe mensaje de redis
-sub.addListener('message', recibeMensaje);
-
-io.sockets.on('connection', function (socket) {
 MongoClient.connect('mongodb://localhost:27017/globalnews', function(err, db) {
     if(err) throw err;
-    //Usuario conectado
-    index_sessionid = socket.handshake.headers.cookie.indexOf('sessionid=');
 
-    //Arreglo para guardar ids del usuario
-    var session_id = socket.handshake.headers.cookie.slice(index_sessionid + 10, index_sessionid + 10 +32);
+    //Subscribe a los diferentes canales
+    sub.subscribe('chat');
+    sub.subscribe('publicacion');
+    sub.subscribe('comentario');
+    sub.subscribe('nuevo_seguidor');
+    sub.subscribe('me_gusta');
 
-    //var session_id = socket.handshake.headers.cookie.slice(-32);
-    obj_usuario = {
-        'session_id': session_id,
-        'socket_id': socket.id,
-        coords: {
-            lat: socket.handshake.query.lat,
-            lon: socket.handshake.query.lon
+    //Funcion que maneja cada evento enviado desde Django
+    var recibeMensaje = function(channel, message){
+        var i,j;
+        var datos = JSON.parse(message);
+        switch (channel) {
+            case 'chat':
+                io.to('chat_' + datos.id_chat ).emit('mensaje_entrada', datos);
+                break;
+
+            case 'publicacion':
+                for(j=0; j<datos.lista_usuarios.length; j++){            
+                    for(i=0; i<usuarios.length; i++){
+                        if(usuarios[i].session_id == datos.lista_usuarios[j]){
+                            io.to(usuarios[i].socket_id).emit('nueva_publicacion', datos);
+                        }
+                    }
+                }
+                db.collection('usuarios').geoNear(Number(datos.longitud), Number(datos.latitud), 
+                    {
+                        maxDistance: 0.0005,
+                        spherical: true,
+                        distanceMultiplier: 6371
+                    }, function(err, result) {
+                    if(err) return console.dir(err)
+
+                    result.results.map(function(resultado){
+                        console.dir(resultado.obj.loc.coordinates);
+                        console.dir(Number(datos.longitud));
+                        console.dir(Number(datos.latitud));
+                        io.to(resultado.obj._id).emit('nueva_publicacion_localizacion', datos);
+                    });
+                }); 
+                break;
+
+            case 'comentario':
+                for(i=0; i<usuarios.length; i++){
+                    if(usuarios[i].session_id == datos.session_key){
+                        io.to(usuarios[i].socket_id).emit('nuevo_comentario', datos);
+                    }
+                }
+                break;
+
+            case 'me_gusta':
+                for(i=0; i<usuarios.length; i++){
+                    if(usuarios[i].session_id == datos.session_key){
+                        io.to(usuarios[i].socket_id).emit('me_gusta', datos);
+                    }
+                }
+                break;
+
+            case 'nuevo_seguidor':
+                for(i=0; i<usuarios.length; i++){
+                    if(usuarios[i].session_id == datos.session_key){
+                        io.to(usuarios[i].socket_id).emit('nuevo_seguidor', datos);
+                    }
+                }
+                break;
         }
     };
+    sub.addListener('message', recibeMensaje);
 
-    usuarios.push(obj_usuario);
+    //Evento que recibe mensaje de redis
+
+    io.sockets.on('connection', function (socket) {
+
+
+        //Usuario conectado
+        index_sessionid = socket.handshake.headers.cookie.indexOf('sessionid=');
+
+        //Arreglo para guardar ids del usuario
+        var session_id = socket.handshake.headers.cookie.slice(index_sessionid + 10, index_sessionid + 10 +32);
+
+        //var session_id = socket.handshake.headers.cookie.slice(-32);
+        obj_usuario = {
+            'session_id': session_id,
+            'socket_id': socket.id,
+            coords: {
+                lat: socket.handshake.query.lat,
+                lon: socket.handshake.query.lon
+            }
+        };
+
+        usuarios.push(obj_usuario);
         var doc = {
             '_id': obj_usuario.socket_id,
             "loc" : {
@@ -103,58 +120,55 @@ MongoClient.connect('mongodb://localhost:27017/globalnews', function(err, db) {
             if(err) throw err;
         });
 
-    //COnfigurar las cookies para comunicarse con Django
-    io.set('authorization', function(data, accept){
-        if(data.headers.cookie){
-            data.cookie = cookie_reader.parse(data.headers.cookie);
-            return accept(null, true);
-        }
-        return accept('error', false);
-    });
-
-    //Arir chat
-    socket.on('abrir_chat', function(id_chat){
-        console.log('Abrio el chat');
-        console.log(id_chat);
-        canal_chat = 'chat_' + id_chat;
-        socket.join(canal_chat);
-    });
-
-    //Nuevo mensaje en el chat
-    socket.on('nuevo_mensaje_chat', function(data){
-
-        valores = querystring.stringify(data);
-
-        var options = {
-            host: host,
-            port: 8000,
-            path: '/nuevo/mensaje/',
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-                'Content-Length': valores.length
+        //COnfigurar las cookies para comunicarse con Django
+        io.set('authorization', function(data, accept){
+            if(data.headers.cookie){
+                data.cookie = cookie_reader.parse(data.headers.cookie);
+                return accept(null, true);
             }
-        };        
-
-        //Enviar mensaje a Django y termina el request
-        var req = http.get(options, function(res){
-            res.setEncoding('utf8');
+            return accept('error', false);
         });
-        req.write(valores);
-        req.end();
-    });
 
-    // Desconectar al usuario y sacarlo de la lista de usuarios
-    socket.on('disconnect', function (socket) {
-        for(var i=0; i<usuarios.length; i++){
-            if(usuarios[i].socket_id == this.id){
-                usuarios.splice(i, 1);
+        //Arir chat
+        socket.on('abrir_chat', function(id_chat){
+            canal_chat = 'chat_' + id_chat;
+            socket.join(canal_chat);
+        });
+
+        //Nuevo mensaje en el chat
+        socket.on('nuevo_mensaje_chat', function(data){
+
+            valores = querystring.stringify(data);
+
+            var options = {
+                host: host,
+                port: 8000,
+                path: '/nuevo/mensaje/',
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                    'Content-Length': valores.length
+                }
+            };        
+
+            //Enviar mensaje a Django y termina el request
+            var req = http.get(options, function(res){
+                res.setEncoding('utf8');
+            });
+            req.write(valores);
+            req.end();
+        });
+
+        // Desconectar al usuario y sacarlo de la lista de usuarios
+        socket.on('disconnect', function (socket) {
+            for(var i=0; i<usuarios.length; i++){
+                if(usuarios[i].socket_id == this.id){
+                    usuarios.splice(i, 1);
+                }
             }
-        }
-        console.log(this.id);
-        db.collection('usuarios').remove({"_id": String(this.id)}, function(err, removed) {
-            if(err) throw err;
+            db.collection('usuarios').remove({"_id": String(this.id)}, function(err, removed) {
+                if(err) throw err;
+            });
         });
     });
-});
 });
