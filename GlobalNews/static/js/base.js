876513cfe073dbeb8eca16a865f0c05f5b197d61
CrisTowi <christian_consuelo@outlook.com>
2015-05-10 19:24:32 -0500

    Arreglar Geolocalizacion en Noticias En tiempo real

--- GlobalNews/static/js/base.js
+++ GlobalNews/static/js/base.js
@@ -1,17 +1,189 @@
 var host = "localhost";
 
-
-
 if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(function(position){
         lat = position.coords.latitude;
         lon = position.coords.longitude;
+        var left_right = 1;
 
         parameters = {
           lat: lat,
           lon: lon
         }
         var socket = io.connect(host + ":3000", {query:parameters});
+
+        $('.btn-abre-platica').on('click', function(e){
+            e.preventDefault();
+            var alerta = $('#alerta-mensaje');
+            alerta.hide();
+
+            $("#btn-enviar").off('click');
+            $('#chat').html("");
+
+            var id_usuario = $(this).data("usuario");
+            var username_chat = $(this).data("username");
+            var imagen = $(this).data("imagen");
+            var username_local =  $(this).data("user");
+            var usuario = $('#input_usuario').val();
+            var media_url = $('#media_url').val();
+            var imagen_local = $(this).data("userfoto");
+
+            $('#nombre-usuario-chat').html(username_chat);
+            var id_chat = $(this).data("chat");
+            socket.emit('abrir_chat', id_chat);
+
+
+
+            var mensaje_entrada = function(objeto){
+               var imagen_url = media_url + imagen_local
+                        if(objeto.usuario_remitente == id_usuario){ 
+                var imagen_url = media_url + imagen;
+                var template = $('<li class="left clearfix"> \
+                                    <span class="chat-img pull-left"> \
+                                        <img src=" ' + imagen_url + ' " alt="User Avatar" width="50px" class="img-circle" /> \
+                                    </span> \
+                                    <div class="chat-body clearfix"> \
+                                        <div class="header"> \
+                                            <strong class="primary-font">' + username_chat + '</strong> \
+                                            <small class="pull-right text-muted"> \
+                                                <i class="fa fa-clock-o fa-fw"></i> ' + objeto.fecha + ' \
+                                            </small> \
+                                        </div> \
+                                        <p> \
+                                            ' + objeto.contenido + ' \
+                                        </p> \
+                                    </div> \
+                                </li>');
+                        } else {
+                var imagen_url = media_url + imagen_local
+                var template = $('<li class="right clearfix"> \
+                                    <span class="chat-img pull-right"> \
+                                        <img src="' + imagen_url + '" alt="User Avatar" width="50px" class="img-circle" /> \
+                                    </span> \
+                                    <div class="chat-body clearfix"> \
+                                        <div class="header"> \
+                                            <small class=" text-muted"> \
+                                                <i class="fa fa-clock-o fa-fw"></i>' + objeto.fecha + '</small> \
+                                            <strong class="pull-right primary-font">' + username_local + '</strong> \
+                                        </div> \
+                                        <p> \
+                                            '+ objeto.contenido +' \
+                                        </p> \
+                                        <a href="#" class="eliminar" data-id="' + objeto.id + '">Eliminar</a> \
+                                    </div> \
+                                </li>');
+                        }
+
+
+
+                   template.hide();
+                   $('#chat').append(template);
+                   template.show('slow');
+
+
+                   $('#mensaje').val('');      
+            }
+
+            socket.addEventListener('mensaje_entrada', mensaje_entrada);
+
+
+            $('#ModalMD').on('hidden.bs.modal', function () {
+                socket.removeEventListener('mensaje_entrada', mensaje_entrada);
+            });
+
+            obtenerChat(id_usuario, username_chat, imagen, username_local, usuario, media_url, imagen_local);
+            $('#btn-enviar').on('click', function(){
+                var mensaje = $('#mensaje').val();
+                if (mensaje == '') {
+                  alerta.fadeIn('slow');
+                  setTimeout(function() {
+                    alerta.fadeOut('slow');
+                  }, 3000);
+                } else {
+                  var info = {usuario_consultor: usuario , usuario_chat: id_usuario, mensaje: mensaje, id_chat: id_chat};
+                  socket.emit('nuevo_mensaje_chat', info);
+                }
+            });
+
+            $(document).on("click", ".eliminar", function(){
+              event.preventDefault();
+              var id = $(this).data("id");
+                $.get( "/eliminar/mensaje/", {id: id}, function(data){
+                  $('#chat').html("");
+                  var n = noty({
+                      text: 'Mensaje eliminado con éxico',
+                      layout: 'bottomRight',
+                      type: 'success',
+                      timeout: 3500,
+                      callback: { onClose: function() { /* Go to the URL i want to go */ }},
+                      animation: {
+                          open: {height: 'toggle'}, // jQuery animate function property object
+                          close: {height: 'toggle'}, // jQuery animate function property object
+                          easing: 'swing', // easing
+                          speed: 500 // opening & closing animation speed
+                      }
+                  });
+                  $(document).unbind("click", ".eliminar");
+                  obtenerChat(id_usuario, username_chat, imagen, username_local, usuario, media_url, imagen_local);
+                });
+            });
+        });
+
+        function obtenerChat(id_usuario, username_chat, imagen, username_local, usuario, media_url, imagen_local){
+        $.ajax({
+                  url: "/chat/",
+                  type: "GET", //send it through get method
+                  data: {usuario_consultor: usuario , usuario_chat: id_usuario},
+                  success: function(response) {
+            
+                    $.each(response, function(i, objeto){
+
+
+
+                        if(objeto.usuario_remitente_id == id_usuario){ 
+                var imagen_url = media_url + imagen;
+                var template = '<li class="left clearfix"> \
+                                    <span class="chat-img pull-left"> \
+                                        <img src=" ' + imagen_url + ' " alt="User Avatar" width="50px" class="img-circle" /> \
+                                    </span> \
+                                    <div class="chat-body clearfix"> \
+                                        <div class="header"> \
+                                            <strong class="primary-font">' + username_chat + '</strong> \
+                                            <small class="pull-right text-muted"> \
+                                                <i class="fa fa-clock-o fa-fw"></i> ' + objeto.fecha + ' \
+                                            </small> \
+                                        </div> \
+                                        <p> \
+                                            ' + objeto.contenido + ' \
+                                        </p> \
+                                    </div> \
+                                </li>';
+                        } else {
+                var imagen_url = media_url + imagen_local
+                var template = '<li class="right clearfix"> \
+                                    <span class="chat-img pull-right"> \
+                                        <img src="' + imagen_url + '" alt="User Avatar" width="50px" class="img-circle" /> \
+                                    </span> \
+                                    <div class="chat-body clearfix"> \
+                                        <div class="header"> \
+                                            <small class=" text-muted"> \
+                                                <i class="fa fa-clock-o fa-fw"></i>' + objeto.fecha + '</small> \
+                                            <strong class="pull-right primary-font">' + username_local + '</strong> \
+                                        </div> \
+                                        <p> \
+                                            '+ objeto.contenido +' \
+                                        </p> \
+                                        <a href="#" class="eliminar" data-id="' + objeto.id + '">Eliminar</a> \
+                                    </div> \
+                                </li>';
+                        }
+                    $('#chat').prepend(template);
+                });
+              }
+        });
+        }
+
+
         //Cuando llegue un nuevo mensaje
         socket.on('nuevo_comentario', function(data){
             var n = noty({
@@ -67,7 +239,6 @@ if (navigator.geolocation) {
                 window.location.href = '/perfil/' + $(this).data('id_usuario');
             });
         });
-        var left_right = 1;
         
         socket.on('nueva_publicacion', function(data){
 
@@ -101,16 +272,6 @@ if (navigator.geolocation) {
             $('#timeline').prepend(template);
             template.show('slow');
 
-
-            var marker1 = new google.maps.Marker({
-                position: new google.maps.LatLng(data.latitud,data.longitud),
-                map: map,
-                title: data.titulo,
-                url: '/publicacion/' + data.id
-                });
-                google.maps.event.addListener(marker1, 'click', function() {
-                window.location.href = this.url;
-            });
             var n = noty({
                 text: '<span class="texto_noty" data-id_nota="' + data.id + '"> Una noticia que te puede interesar </span>',
                 layout: 'bottomRight',
@@ -125,6 +286,15 @@ if (navigator.geolocation) {
             });
             $('.texto_noty').on('click', function(){
                 window.location.href = '/publicacion/' + $(this).data('id_nota');
+            });
+            var marker1 = new google.maps.Marker({
+                position: new google.maps.LatLng(data.latitud,data.longitud),
+                map: map,
+                title: data.titulo,
+                url: '/publicacion/' + data.id
+                });
+                google.maps.event.addListener(marker1, 'click', function() {
+                window.location.href = this.url;
             });
         });
         socket.on('nueva_publicacion_localizacion', function(data){
@@ -159,16 +329,6 @@ if (navigator.geolocation) {
             $('#timeline').prepend(template);
             template.show('slow');
 
-
-            var marker1 = new google.maps.Marker({
-                position: new google.maps.LatLng(data.latitud,data.longitud),
-                map: map,
-                title: data.titulo,
-                url: '/publicacion/' + data.id
-                });
-                google.maps.event.addListener(marker1, 'click', function() {
-                window.location.href = this.url;
-            });
             var n = noty({
                 text: '<span class="texto_noty" data-id_nota="' + data.id + '"> Una nueva noticia cercana a ti </span>',
                 layout: 'bottomRight',
@@ -184,7 +344,15 @@ if (navigator.geolocation) {
             $('.texto_noty').on('click', function(){
                 window.location.href = '/publicacion/' + $(this).data('id_nota');
             });
-
+            var marker1 = new google.maps.Marker({
+                position: new google.maps.LatLng(data.latitud,data.longitud),
+                map: map,
+                title: data.titulo,
+                url: '/publicacion/' + data.id
+                });
+                google.maps.event.addListener(marker1, 'click', function() {
+                window.location.href = this.url;
+            });
         });
     });
 }
